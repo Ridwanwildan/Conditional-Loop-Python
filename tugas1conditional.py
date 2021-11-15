4fc3c89f058d88e5797d3d0f9cea0db0ea4d4d18
Ridwanwildan <rdwnwldn@gmail.com>
2021-11-15 10:50:10 +0700

    Tugas 1: Menentukan dua bilangan terbesar

---
+++ tugas1conditional.py
@@ -0,0 +1,7 @@
+a = int(input("Masukkan angka pertama :"))
+b = int(input("Masukkan angka kedua :"))
+
+if a > b:
+     print("Bilangan terbesar yaitu :",a)
+if b > a:
+     print("Bilangan terbesar yaitu :",b)
